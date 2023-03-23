6b17cb650c41e4b42ec8c1ed62613a52e3b576c8
Deepa Masilamani <102405405+dschennai@users.noreply.github.com>
2023-03-23 13:22:14 +0530

    Update Audio2TextConverter.py

--- Audio2TextConverter.py
+++ Audio2TextConverter.py
@@ -108,7 +108,7 @@ def save_audio_files_to_folder(audiofile):
             save_path = Path("audio") / audiofile.name
             audio.export(save_path, format=file_type)
             filename_returned = audiofile.name
-        elif audiofile.name.endswith('mp3') or audiofile.name.endswith('mpga')or audiofile.name.endswith('m4a'):
+        elif audiofile.name.endswith('mp3') or audiofile.name.endswith('mpga') or audiofile.name.endswith('m4a'):
             file_name_without_extension = os.path.splitext(audiofile.name)[0]
             file_name_without_extension = file_name_without_extension + ".wav"
             audio = pydub.AudioSegment.from_mp3(audiofile)
